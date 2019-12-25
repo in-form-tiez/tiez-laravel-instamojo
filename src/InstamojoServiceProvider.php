612943490fc32018ef0d83fd66fc01bacc71cc72
Sachin <sachin.soni@tiez.nl>
2019-12-25 12:46:58 +0530

    added version

--- src/InstamojoServiceProvider.php
+++ src/InstamojoServiceProvider.php
@@ -5,12 +5,22 @@ use Illuminate\Support\ServiceProvider;
 
 class InstamojoServiceProvider extends ServiceProvider{
 
+	/**
+    * Bootstrap services.
+    *
+    * @return void
+    */
 	public function boot(){
 
 		$this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 	}
 
+	/**
+    * Register services.
+    *
+    * @return void
+    */
 	public function register(){
 
 		// register our controller
